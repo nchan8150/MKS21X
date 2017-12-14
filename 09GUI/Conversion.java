914ee3e4ab5f6c4753b5ca6e2f7fba047659e482
nchan8150 <nchan1@stuy.edu>
2017-12-13 21:39:52 -0500

    add 09

--- 09GUI/Conversion.java
+++ 09GUI/Conversion.java
@@ -1,49 +1,56 @@
-import javac.swing.*;
+import javax.swing.*;
 import java.awt.*;
-import java.swing.event.*;
+import java.awt.event.*;
 
 public class Conversion extends JFrame implements ActionListener{
-
-    public static double CtoF(double t) {
-	return t * 5 / 9 + 32;
-    }
-
-    public static double FtoC(double t) {
-	return (t - 32) * 5 / 9;
-    }
     
-    private Conttainer pane;
-    private Jbutton fb;
-    private JTextField ft;
-    private Jbutton cb;
-    private JTextField ct;
+    private Container pane;
+    private JTextField t;
+    private JButton f;
+    private JButton c;
 
+    public void actionPerformed (ActionEvent e) {
+	String s = e.getActionCommand();
+	if (s.equals("Convert from F to C")) {
+	    System.out.println(FtoC(Double.parseDouble(t.getText())));
+	}
+	else if (s.equals("Convert from C to F")) {
+	    System.out.println(CtoF(Double.parseDouble(t.getText())));
+	}
+    }
+	    
     public Conversion() {
-	this.setTitle("Convering Fahrenheit and Celsuis");
-	this.setSize(600,400);
+	this.setTitle("Converting Fahrenheit and Celsuis");
+	this.setSize(600,100);
 	this.setLocation(100,100);
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-	pane.this.getContentPane();
+	pane = this.getContentPane();
 	pane.setLayout(new FlowLayout());
+
+	t = new JTextField(12);
+	f = new JButton("Convert from F to C");
+	c = new JButton("Convert from C to F");
+
+	t.addActionListener(this);
+	f.addActionListener(this);
+	c.addActionListener(this);
 	
-	fb = new JButton("Convert from F to C");
-	ft = new JTextField(12);
-	cb = new JButton("Convert from C to F");
-	ct = new JTextField(12);
-	pane.add(fb);
-	pane.add(ft);
-	pane.add(cb);
-	pane.add(ct);
- }
-    
-    /*
-    public static void main(String[] args) {
-	System.out.println(CtoF(0)); //32
-	System.out.println(CtoF(-50)); //-58
-	System.out.println(FtoC(212)); //100
-	System.out.println(FtoC(21.2)); //-6
-	System.out.println(FtoC(180)); //82.22
+	pane.add(t);
+	pane.add(f);
+	pane.add(c);
     }
-    */
+    
+    public static double CtoF(double t) {
+	return t * 9.0 / 5 + 32;
+    }
+
+    public static double FtoC(double t) {
+	return (t - 32.0) * 5 / 9;
+    }
+
+    public static void main (String[] args) {
+	Conversion x = new Conversion();
+	x.setVisible(true);
+    }
 }
